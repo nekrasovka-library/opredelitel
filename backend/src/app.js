8a4fe6f84872961529b4f2181672535be4fdb354
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-03-11 19:42:24 +0300

    Удален обработчик статических файлов для изображений.

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,6 @@ const setup = require("./middlewares/setup");
 
 // Constants for paths
 const PATH_TO_FRONTEND = join(__dirname, "../../frontend/build");
-const PATH_TO_IMAGES = join(__dirname, "../../images");
 const INDEX_HTML_PATH = join(PATH_TO_FRONTEND, "index.html");
 
 const app = express();
@@ -14,7 +13,6 @@ setup(app, {
   router,
   paths: {
     frontend: PATH_TO_FRONTEND,
-    images: PATH_TO_IMAGES,
   },
 });
 
--- backend/src/middlewares/setup.js
+++ backend/src/middlewares/setup.js
@@ -6,7 +6,6 @@ function setup(app, { router, paths }) {
   app.use(express.json()); // Парсер для JSON
   app.use(cors()); // CORS-защита
   app.use("/api", router); // Маршруты API
-  app.use("/images", expressStatic(paths.images)); // Статические файлы из папки с изображениями
   app.use(expressStatic(paths.frontend)); // Статические файлы фронтенда
 }
 
