2956ab5158c78c8963581567d8fe5de01673073a
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-03-11 14:13:05 +0300

    Добавлен модуль CORS и его подключение в app.js.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes");
 const { join } = require("node:path");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", router);
 app.use("/images", express.static(join(__dirname, "../../images")));
