63863cd56e6c22c1a94099abe677b58951eaa626
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-03-11 14:28:53 +0300

    Рефакторинг `app.js`: упрощена настройка мидлваров, добавлена отдача статических файлов и обработка всех маршрутов.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,29 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const router = require("./routes");
 const { join } = require("node:path");
 const cors = require("cors");
+const router = require("./routes");
+
+// Constants for paths
+const PATH_TO_FRONTEND = join(__dirname, "../../frontend/build");
+const PATH_TO_IMAGES = join(__dirname, "../../images");
+const INDEX_HTML_PATH = join(PATH_TO_FRONTEND, "index.html");
 
 const app = express();
-app.use(bodyParser.json());
-app.use(express.json());
-app.use(cors());
 
-app.use("/api", router);
-app.use("/images", express.static(join(__dirname, "../../images")));
+// Function to set up middlewares
+function setupMiddlewares(app) {
+  app.use(express.json());
+  app.use(cors());
+  app.use("/api", router);
+  app.use("/images", express.static(PATH_TO_IMAGES));
+  app.use(express.static(PATH_TO_FRONTEND));
+}
+
+setupMiddlewares(app);
+
+// Serve index file
+app.get("*", (req, res) => {
+  res.sendFile(INDEX_HTML_PATH);
+});
 
 module.exports = app;
