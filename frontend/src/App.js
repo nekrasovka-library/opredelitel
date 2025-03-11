d8c6dde95eb730b82996a222790f7b565e000bb1
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-03-11 12:29:27 +0300

    "Удален весь исходный код проекта React"

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import { IconButton } from "./components/Button/button.styles";
+import Icon from "./components/Icon";
+import AlphabetNavigator from "./components/AlphabetNavigator";
+import React from "react";
+import Blocks from "./container/Blocks";
+import Description from "./container/Description";
+import Title from "./container/Title";
+import About from "./container/About";
+import How from "./container/How";
+import { OpredelitelProvider } from "./context";
+import Resume from "./container/Resume";
+
+function Opredelitel() {
+  const handleScrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <OpredelitelProvider>
+      <AlphabetNavigator />
+      <Title />
+      <Description />
+      <How />
+      <About />
+      <Blocks />
+      <Resume />
+      <IconButton onClick={handleScrollTop}>
+        <Icon icon="arrowUp" />
+      </IconButton>
+    </OpredelitelProvider>
+  );
+}
+
+export default Opredelitel;
