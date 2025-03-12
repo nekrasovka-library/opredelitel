694fb989f7847c3bcdc49131a66d3402fbbde8fc
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-03-12 07:44:37 +0300

    Удален обработчик статических файлов для изображений.

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ const setup = require("./middlewares/setup");
 
 // Constants for paths
 const PATH_TO_FRONTEND = join(__dirname, "../../frontend/build");
+const PATH_TO_IMAGES = join(__dirname, "../../images");
 const INDEX_HTML_PATH = join(PATH_TO_FRONTEND, "index.html");
 
 const app = express();
@@ -13,6 +14,7 @@ setup(app, {
   router,
   paths: {
     frontend: PATH_TO_FRONTEND,
+    images: PATH_TO_IMAGES,
   },
 });
 
