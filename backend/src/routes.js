8878a58bbbd8ca525ead0d0d3d51274ace5d80db
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-03-12 08:11:34 +0300

    Заменена обработка изображений через Worker на использование библиотеки Sharp для оптимизации и упрощения кода. Исправлена логика загрузки изображений на фронтенде.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const { Router } = require("express");
 const { Worker } = require("worker_threads"); // Подключение многопоточности
 const router = Router();
 const { join } = require("path");
+const sharp = require("sharp");
 
 const CACHE_PATH = path.join(__dirname, ".cache"); // Кэш обработанных изображений
 
@@ -11,7 +12,7 @@ if (!fs.existsSync(CACHE_PATH)) {
   fs.mkdirSync(CACHE_PATH, { recursive: true }); // Создаем папку для кэша, если её нет
 }
 
-router.get("/optimized-images/:imageName", (req, res) => {
+router.get("/optimized-images/:imageName", async (req, res) => {
   const { imageName } = req.params;
   const originalImagePath = join(__dirname, "../../images", imageName);
   const cachedImagePath = path.join(
@@ -19,46 +20,74 @@ router.get("/optimized-images/:imageName", (req, res) => {
     `${path.parse(imageName).name}.webp`,
   );
 
-  // Если кэшированное изображение уже существует - вернуть его
-  if (fs.existsSync(cachedImagePath)) {
+  try {
+    if (!fs.existsSync(originalImagePath)) {
+      console.error(`Original file not found at: ${originalImagePath}`);
+      return res.status(404).send("Original image not found");
+    }
+
+    // Если кэш уже существует, отдаем кэшированное изображение
+    if (fs.existsSync(cachedImagePath)) {
+      res.set("Content-Type", "image/webp");
+      return fs.createReadStream(cachedImagePath).pipe(res);
+    }
+
+    // Обработка через sharp
+    const optimizedImageBuffer = await sharp(originalImagePath)
+      .resize({ height: 250 })
+      .toFormat("webp")
+      .toBuffer();
+
+    // Сохраняем обработанное изображение
+    fs.writeFileSync(cachedImagePath, optimizedImageBuffer);
+
     res.set("Content-Type", "image/webp");
-    return fs.createReadStream(cachedImagePath).pipe(res);
+    res.send(optimizedImageBuffer);
+  } catch (error) {
+    console.error("Error processing image:", error);
+    res.status(500).send("An error occurred while processing the image");
   }
 
-  // Если картинки в кэше нет, обрабатываем через воркер
-  const worker = new Worker(
-    path.join(__dirname, "/helpers/imageProcessor.js"),
-    {
-      workerData: { originalImagePath, cachedImagePath }, // Передача данных в воркер
-    },
-  );
-
-  // Флаг, чтобы исключить несколько вызовов res.send
-  let responseSent = false;
-
-  worker.on("message", () => {
-    if (!responseSent) {
-      responseSent = true; // Устанавливаем флаг
-      res.set("Content-Type", "image/webp");
-      fs.createReadStream(cachedImagePath).pipe(res);
-    }
-  });
-
-  worker.on("error", (err) => {
-    if (!responseSent) {
-      responseSent = true; // Устанавливаем флаг
-      console.error("Ошибка обработки изображения", err);
-      res.status(500).send("Ошибка обработки изображения.");
-    }
-  });
-
-  worker.on("exit", (code) => {
-    if (code !== 0 && !responseSent) {
-      responseSent = true; // Устанавливаем флаг
-      console.error(`Воркер завершился с кодом ${code}`);
-      res.status(500).send("Ошибка обработки изображения.");
-    }
-  });
+  // // Если кэшированное изображение уже существует - вернуть его
+  // if (fs.existsSync(cachedImagePath)) {
+  //   res.set("Content-Type", "image/webp");
+  //   return fs.createReadStream(cachedImagePath).pipe(res);
+  // }
+  //
+  // // Если картинки в кэше нет, обрабатываем через воркер
+  // const worker = new Worker(
+  //   path.join(__dirname, "/helpers/imageProcessor.js"),
+  //   {
+  //     workerData: { originalImagePath, cachedImagePath }, // Передача данных в воркер
+  //   },
+  // );
+  //
+  // // Флаг, чтобы исключить несколько вызовов res.send
+  // let responseSent = false;
+  //
+  // worker.on("message", () => {
+  //   if (!responseSent) {
+  //     responseSent = true; // Устанавливаем флаг
+  //     res.set("Content-Type", "image/webp");
+  //     fs.createReadStream(cachedImagePath).pipe(res);
+  //   }
+  // });
+  //
+  // worker.on("error", (err) => {
+  //   if (!responseSent) {
+  //     responseSent = true; // Устанавливаем флаг
+  //     console.error("Ошибка обработки изображения", err);
+  //     res.status(500).send("Ошибка обработки изображения.");
+  //   }
+  // });
+  //
+  // worker.on("exit", (code) => {
+  //   if (code !== 0 && !responseSent) {
+  //     responseSent = true; // Устанавливаем флаг
+  //     console.error(`Воркер завершился с кодом ${code}`);
+  //     res.status(500).send("Ошибка обработки изображения.");
+  //   }
+  // });
 });
 
 module.exports = router;
--- frontend/src/components/Image/index.jsx
+++ frontend/src/components/Image/index.jsx
@@ -25,65 +25,64 @@ const Image = ({ imageUrl, className }) => {
       if (blockRef.current) observer.unobserve(blockRef.current);
     };
   }, []);
-  //
-  // useEffect(() => {
-  //   if (isVisible && !imageLoaded) {
-  //     // Если изображение становится видимым и еще не загружено
-  //     const abortController = new AbortController();
-  //     setLoadingAbortController(abortController);
-  //
-  //     // Используем fetch для загрузки изображения
-  //     fetch(imageUrl, { signal: abortController.signal })
-  //       .then((response) => {
-  //         if (!response.ok) {
-  //           throw new Error("Ошибка загрузки изображения");
-  //         }
-  //         return response.blob(); // Преобразуем ответ в blob
-  //       })
-  //       .then((blob) => {
-  //         const blobUrl = URL.createObjectURL(blob);
-  //         setImageBlob(blobUrl); // Устанавливаем URL загруженного blob
-  //         setImageLoaded(true);
-  //         setLoadingAbortController(null); // Очищаем AbortController
-  //       })
-  //       .catch((error) => {
-  //         if (error.name === "AbortError") {
-  //           console.log("Загрузка изображения прервана");
-  //         } else {
-  //           console.error("Ошибка загрузки:", error);
-  //         }
-  //         setLoadingAbortController(null);
-  //       });
-  //
-  //     return () => {
-  //       if (abortController) {
-  //         // Прерываем загрузку, если компонент уходит из зоны видимости
-  //         abortController.abort();
-  //       }
-  //     };
-  //   } else if (!isVisible && loadingAbortController) {
-  //     // Если компонент выходит из зоны видимости, прерываем загрузку
-  //     loadingAbortController.abort();
-  //     setLoadingAbortController(null);
-  //   }
-  // }, [isVisible]);
-  //
-  // useEffect(() => {
-  //   return () => {
-  //     if (imageBlob) {
-  //       URL.revokeObjectURL(imageBlob);
-  //       setImageBlob(null);
-  //     }
-  //   };
-  // }, [imageBlob]);
+
+  useEffect(() => {
+    if (isVisible && !imageLoaded) {
+      // Если изображение становится видимым и еще не загружено
+      const abortController = new AbortController();
+      setLoadingAbortController(abortController);
+
+      // Используем fetch для загрузки изображения
+      fetch(imageUrl, { signal: abortController.signal })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Ошибка загрузки изображения");
+          }
+          return response.blob(); // Преобразуем ответ в blob
+        })
+        .then((blob) => {
+          const blobUrl = URL.createObjectURL(blob);
+          setImageBlob(blobUrl); // Устанавливаем URL загруженного blob
+          setImageLoaded(true);
+          setLoadingAbortController(null); // Очищаем AbortController
+        })
+        .catch((error) => {
+          if (error.name === "AbortError") {
+            console.log("Загрузка изображения прервана");
+          } else {
+            console.error("Ошибка загрузки:", error);
+          }
+          setLoadingAbortController(null);
+        });
+
+      return () => {
+        if (abortController) {
+          // Прерываем загрузку, если компонент уходит из зоны видимости
+          abortController.abort();
+        }
+      };
+    } else if (!isVisible && loadingAbortController) {
+      // Если компонент выходит из зоны видимости, прерываем загрузку
+      loadingAbortController.abort();
+      setLoadingAbortController(null);
+    }
+  }, [isVisible]);
+
+  useEffect(() => {
+    return () => {
+      if (imageBlob) {
+        URL.revokeObjectURL(imageBlob);
+        setImageBlob(null);
+      }
+    };
+  }, [imageBlob]);
 
   return (
     <ImageStyles
       isVisible={isVisible}
       className={className}
       ref={blockRef}
-      // imageUrl={imageBlob ? `url(${imageBlob})` : "none"}
-      imageUrl={`url(${imageUrl})`}
+      imageUrl={imageBlob ? `url(${imageBlob})` : "none"}
     >
       <div>
         <div />
