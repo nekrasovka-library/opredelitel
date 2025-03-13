6a95cdd02c04e0bbd3bdb6aa8381d006206a90eb
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-03-13 12:04:16 +0300

    "Добавлена загрузка изображений с использованием контекста и оптимизировано управление состоянием в компоненте Image"

--- frontend/src/components/Image/index.jsx
+++ frontend/src/components/Image/index.jsx
@@ -1,24 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { ImageStyles } from "./image.styles";
+import { OpredelitelContext } from "../../context";
 
 const Image = ({ imageUrl, className, isIntersected, setIsIntersected }) => {
+  const { setImagesToLoad, loadedImages } = useContext(OpredelitelContext);
+
   const API_URL = process.env.REACT_APP_API_URL;
-  const DEFAULT_IMAGE = `${API_URL}/api/optimized-images/20/${imageUrl}`;
-  const LARGE_IMAGE = `${API_URL}/images/${imageUrl}`;
+  const SMALL_IMAGE = `${API_URL}/api/optimized-images/20/${imageUrl}`;
   const blockRef = useRef(null); // Ссылка на HTML-элемент
   const [isFullImageLoaded, setIsFullImageLoaded] = useState(false); // Флаг завершённой загрузки изображения (чтобы не грузить снова)
   const visibilityTimerRef = useRef(null); // Таймер на 200 мс
-
-  const loadImage = () => {
-    const img = document.createElement("img");
-    img.src = LARGE_IMAGE;
-
-    img.onload = () => {
-      const div = blockRef.current.children[0].children[0];
-      div.style.backgroundImage = `url(${LARGE_IMAGE})`;
-      setIsFullImageLoaded(true);
-    };
-  };
 
   useEffect(() => {
     const currentBlockRef = blockRef.current;
@@ -32,7 +23,8 @@ const Image = ({ imageUrl, className, isIntersected, setIsIntersected }) => {
         if (entry.isIntersecting) {
           if (!isFullImageLoaded) {
             visibilityTimerRef.current = setTimeout(() => {
-              loadImage();
+              // loadImage();
+              setImagesToLoad((prevState) => [...prevState, imageUrl]);
             }, 200); // Ожидаем, пока блок находится в видимости 200 мс
           }
         } else clearTimeout(visibilityTimerRef.current);
@@ -54,12 +46,24 @@ const Image = ({ imageUrl, className, isIntersected, setIsIntersected }) => {
     };
   }, [imageUrl, isFullImageLoaded]);
 
+  useEffect(() => {
+    if (isFullImageLoaded) return;
+
+    const imageObject = loadedImages.find((image) => image.id === imageUrl);
+
+    if (imageObject) {
+      const div = blockRef.current.children[0].children[0];
+      div.style.backgroundImage = `url(${imageObject.path})`;
+      setIsFullImageLoaded(true);
+    }
+  }, [loadedImages, setIsFullImageLoaded, isFullImageLoaded]);
+
   return (
     <ImageStyles
       isIntersected={isIntersected}
       className={className}
       ref={blockRef}
-      imageUrl={`url(${DEFAULT_IMAGE})`}
+      imageUrl={`url(${SMALL_IMAGE})`}
     >
       <div>
         <div />
--- frontend/src/context/index.js
+++ frontend/src/context/index.js
@@ -1,12 +1,77 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useCallback, useEffect, useState } from "react";
 
 // Создаем контекст
 export const OpredelitelContext = createContext();
 
 // Создаем провайдер для контекста
 export const OpredelitelProvider = ({ children }) => {
+  const API_URL = process.env.REACT_APP_API_URL;
+
   const [paperType, setPaperType] = useState(1);
   const [paperSelected, setPaperSelected] = useState("");
+  const [loadedImages, setLoadedImages] = useState([]);
+  const [imagesToLoad, setImagesToLoad] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(true);
+
+  // Функция загрузки изображения с использованием new Image
+  const loadImage = (imageName) => {
+    const imagePath = `${API_URL}/images/${imageName}`;
+
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+
+      // Событие для успешной загрузки изображения
+      img.onload = () => {
+        console.log(`Изображение успешно загружено: ${imageName}`);
+        resolve(imagePath); // Возвращаем путь загруженного изображения
+      };
+
+      // Событие для обработки ошибок загрузки
+      img.onerror = (error) => {
+        console.error(`Ошибка загрузки изображения: ${imageName}`, error);
+        reject(error); // Возвращаем ошибку
+      };
+
+      // Устанавливаем путь
+      img.src = imagePath;
+    });
+  };
+
+  // Основной процесс загрузки изображений
+  const processImages = useCallback(async () => {
+    if (isLoaded && imagesToLoad.length > 0) {
+      // Забираем первое изображение из массива
+      const imageName = imagesToLoad[0];
+      // Ставим флаг "загрузка идет"
+      setIsLoaded(false);
+
+      try {
+        // Реальная загрузка изображения
+        const uploadedPath = await loadImage(imageName);
+
+        // Добавляем в массив загруженных изображений
+        setLoadedImages((prevState) => [
+          ...prevState,
+          { id: imageName, path: uploadedPath },
+        ]);
+
+        // Убираем загруженное изображение из очереди
+        setImagesToLoad((prevState) => prevState.slice(1));
+      } catch (error) {
+        console.error(`Ошибка при обработке ${imageName}:`, error);
+      } finally {
+        // Ставим флаг "загружено"
+        setIsLoaded(true);
+      }
+    }
+  }, [isLoaded, imagesToLoad]);
+
+  // Вызываем процесс загрузки при изменении состояния
+  useEffect(() => {
+    if (imagesToLoad.length > 0) {
+      processImages();
+    }
+  }, [imagesToLoad, processImages]);
 
   return (
     <OpredelitelContext.Provider
@@ -15,6 +80,8 @@ export const OpredelitelProvider = ({ children }) => {
         setPaperType,
         paperSelected,
         setPaperSelected,
+        setImagesToLoad,
+        loadedImages,
       }}
     >
       {children}
