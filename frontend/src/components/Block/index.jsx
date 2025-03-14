7985dedc5cf56a40ba9277a23d8807c5d4aa7754
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-03-14 13:39:53 +0300

    Рефакторинг функций и логики компонента Block: улучшено именование, добавлена обработка кликов по ссылкам, упрощён подсчёт изображений.

--- frontend/src/components/Block/index.jsx
+++ frontend/src/components/Block/index.jsx
@@ -22,74 +22,79 @@ import BlockImage from "../../components/Image";
 const Block = ({ item, id }) => {
   const { paperSelected, setPaperSelected } = useContext(OpredelitelContext);
   const [currentIndex, setCurrentIndex] = useState(null);
-  const [isIntersected, setIsIntersected] = useState(false);
+  const [isImageIntersected, setImageIntersected] = useState(false);
+
+  const TOTAL_IMAGES = item.images.length;
 
   const openImage = (index) => setCurrentIndex(index);
-  const resetCurrentIndex = () => setCurrentIndex(null);
+  const closeImage = () => setCurrentIndex(null);
 
-  const showPreviousImage = () => {
+  const navigateToPreviousImage = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? item.images.length - 1 : prevIndex - 1,
+      prevIndex === 0 ? TOTAL_IMAGES - 1 : prevIndex - 1,
     );
   };
 
-  const showNextImage = () => {
+  const navigateToNextImage = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === item.images.length - 1 ? 0 : prevIndex + 1,
+      prevIndex === TOTAL_IMAGES - 1 ? 0 : prevIndex + 1,
     );
   };
 
-  const togglePaperSelection = (paperId) => {
-    const newId = paperId === paperSelected ? "" : paperId;
-    setPaperSelected(newId);
-    window.history.replaceState({}, "", `/opredelitel/${newId}`);
+  const handlePaperSelectionToggle = () => {
+    const newPaperId = id === paperSelected ? "" : id;
+    setPaperSelected(newPaperId);
+    window.history.replaceState({}, "", `/opredelitel/${newPaperId}`);
+  };
+
+  const handleLinkClick = (event) => {
+    if (event.target.tagName === "A") {
+      event.preventDefault();
+      const lastSegment = event.target.href.split("/").filter(Boolean).pop();
+      setPaperSelected(lastSegment);
+    }
   };
 
   return (
     <BlockStyles id={id}>
       <BlockImage
         imageUrl={item.album}
-        isIntersected={isIntersected}
-        setIsIntersected={setIsIntersected}
+        isIntersected={isImageIntersected}
+        setIsIntersected={setImageIntersected}
       />
+
       <Liner linerHeight={30} borderColor={item.color} />
+
       <BlockRectangularButton
         borderColor={item.color}
-        onClick={() => togglePaperSelection(id)}
+        onClick={handlePaperSelectionToggle}
       >
         {item.name}
       </BlockRectangularButton>
+
       {paperSelected === id && (
         <BlockHidden>
           <BlockDescription>
             <div
-              dangerouslySetInnerHTML={{
-                __html: item.text,
-              }}
-              onClick={(event) => {
-                if (event.target.tagName === "A") {
-                  event.preventDefault();
-                  const pathSegments = event.target.href
-                    .split("/")
-                    .filter(Boolean);
-                  const lastSegment = pathSegments[pathSegments.length - 1];
-                  togglePaperSelection(lastSegment);
-                }
-              }}
+              dangerouslySetInnerHTML={{ __html: item.text }}
+              onClick={handleLinkClick}
             />
           </BlockDescription>
+
           <MasonryGrid images={item.images} openImage={openImage} />
+
           {currentIndex !== null && (
             <Modal>
-              <CloseButton onClick={resetCurrentIndex}>
+              <CloseButton onClick={closeImage}>
                 <Icon icon="close" height={20} fill="#000" />
               </CloseButton>
-              {item.images.length > 1 && (
+
+              {TOTAL_IMAGES > 1 && (
                 <>
-                  <ArrowButtonLeft onClick={showPreviousImage}>
+                  <ArrowButtonLeft onClick={navigateToPreviousImage}>
                     <Icon icon="arrowLeft" height={20} width={20} fill="#000" />
                   </ArrowButtonLeft>
-                  <ArrowButtonRight onClick={showNextImage}>
+                  <ArrowButtonRight onClick={navigateToNextImage}>
                     <Icon
                       icon="arrowRight"
                       height={20}
@@ -99,20 +104,19 @@ const Block = ({ item, id }) => {
                   </ArrowButtonRight>
                 </>
               )}
+
               <FullscreenWrapper>
                 <ImagesContainer currentIndex={currentIndex}>
-                  {item.images.map((image, index) => {
-                    return (
-                      <ImageContainer key={index}>
-                        <ProgressiveImage
-                          isToLoad={index === currentIndex}
-                          imageUrl={image.dataset.original}
-                          alt={image.name}
-                        />
-                        <span>{image.name}</span>
-                      </ImageContainer>
-                    );
-                  })}
+                  {item.images.map((image, index) => (
+                    <ImageContainer key={index}>
+                      <ProgressiveImage
+                        isToLoad={index === currentIndex}
+                        imageUrl={image.dataset.original}
+                        alt={image.name}
+                      />
+                      <span>{image.name}</span>
+                    </ImageContainer>
+                  ))}
                 </ImagesContainer>
               </FullscreenWrapper>
             </Modal>
