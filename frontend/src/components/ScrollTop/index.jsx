7694e53d86049dafd05e9e811fcbd0a0dfbbaa9b
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-03-25 12:24:02 +0300

    Добавлен переход на страницу "/opredelitel/" при наличии blockId в ScrollTop.

--- frontend/src/components/ScrollTop/index.jsx
+++ frontend/src/components/ScrollTop/index.jsx
@@ -3,11 +3,15 @@ import { IconButton } from "../Button/button.styles";
 import Icon from "../Icon";
 import { useElementVisibility } from "../../helpers";
 import { OpredelitelContext } from "../../context";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ScrollTop = () => {
+  const navigate = useNavigate();
+  const { blockId } = useParams();
   const { refMap } = useContext(OpredelitelContext);
   const [isVisible, setIsVisible] = useState(false);
   const handleScrollTop = () => {
+    if (blockId) navigate(`/opredelitel/`);
     window.scrollTo({
       top: 0,
       behavior: "smooth",
