4c42313ad559f7f49b18f11533bc0ad7ee69bd64
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-03-25 12:11:00 +0300

    Исправлены ссылки на favicon и manifest, уточнён DOCTYPE.

--- backend/src/middlewares/setup.js
+++ backend/src/middlewares/setup.js
@@ -7,7 +7,7 @@ function setup(app, { router, paths }) {
   app.use(cors()); // CORS-защита
   app.use("/opredelitel/api", router); // Маршруты API
   app.use("/opredelitel/images", expressStatic(paths.images)); // Статические файлы из папки с изображениями
-  app.use("/opredelitel", expressStatic(paths.frontend)); // Статические файлы фронтенда
+  app.use(expressStatic(paths.frontend)); // Статические файлы фронтенда
 }
 
 module.exports = setup;
