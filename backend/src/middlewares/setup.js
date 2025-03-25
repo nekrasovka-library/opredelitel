173b76658524a31c33d54fd15f1de0a3fb65c201
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-03-25 08:21:16 +0300

    Добавлен middleware для сервировки favicon.

--- backend/src/middlewares/setup.js
+++ backend/src/middlewares/setup.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { join } = require("node:path");
+const favicon = require("serve-favicon");
 const expressStatic = express.static;
 
 function setup(app, { router, paths }) {
@@ -8,6 +10,7 @@ function setup(app, { router, paths }) {
   app.use("/opredelitel/api", router); // Маршруты API
   app.use("/opredelitel/images", expressStatic(paths.images)); // Статические файлы из папки с изображениями
   app.use(expressStatic(paths.frontend)); // Статические файлы фронтенда
+  app.use(favicon(join(paths.frontend, "favicon.ico")));
 }
 
 module.exports = setup;
